1c642c422158758db5b06181df26e7526d722c00
jingyi <zhao.elton@gmail.com>
2024-12-09 17:11:53 -0800

    - add observability to simple postgres

--- packages/vanilla-postgres-swarm/script/write-benchmark.js
+++ packages/vanilla-postgres-swarm/script/write-benchmark.js
@@ -1,10 +1,14 @@
 import clients from '../client.js';
-const concurrentWrites = 50000; // Adjust this number to test different levels of concurrency
+const numberOfConcurrentWrites = 20000; // Adjust this number to test different levels of concurrency
 
 async function performWriteOperation(client, id) {
   const query =
     'INSERT INTO test_table (id, value, timestamp) VALUES ($1, $2, NOW())';
   const values = [id, `value-${id}`];
+
+  // const largeValue = 'x'.repeat(1000); // Example of a larger value
+  // const values = [id, largeValue];
+
   try {
     await client.query(query, values);
     console.log(
@@ -19,15 +23,20 @@ async function concurrentWrite(client, concurrentWrites) {
   await client.connect();
   const writePromises = [];
   console.time('Total write time');
+  const startTime = Date.now();
   for (let i = 0; i < concurrentWrites; i++) {
     writePromises.push(performWriteOperation(client, i));
   }
   await Promise.all(writePromises);
+  const endTime = Date.now();
   console.timeEnd('Total write time');
   await client.end();
+  const totalTimeInSeconds = (endTime - startTime) / 1000;
+  const writesPerSecond = concurrentWrites / totalTimeInSeconds;
   console.log(`Completed ${concurrentWrites} concurrent write operations`);
+  console.log(`Max write speed: ${writesPerSecond.toFixed(2)} writes/second`);
 }
 
 clients.forEach((client) => {
-  concurrentWrite(client, concurrentWrites);
+  concurrentWrite(client, numberOfConcurrentWrites);
 });
